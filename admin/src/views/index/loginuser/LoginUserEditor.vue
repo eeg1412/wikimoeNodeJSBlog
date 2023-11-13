a4f113b1794002e52feb5367d2863890c769d4e8
hiroki <eeg1412@gmail.com>
2023-11-13 10:08:33 +0900

    用户设置增加tabs

--- admin/src/views/index/loginuser/LoginUserEditor.vue
+++ admin/src/views/index/loginuser/LoginUserEditor.vue
@@ -2,70 +2,85 @@
   <div class="common-right-panel-form common-limit-width">
     <h3 class="common-title">用户设置</h3>
     <div>
-      <el-form :model="form" :rules="rules" ref="formRef" label-width="80px">
-        <el-form-item label="昵称" prop="nickname">
-          <el-input v-model="form.nickname" placeholder="请输入昵称"></el-input>
-        </el-form-item>
-        <el-form-item label="邮箱" prop="email">
-          <el-input v-model="form.email" placeholder="请输入邮箱"></el-input>
-        </el-form-item>
-        <el-form-item label="头像" prop="photo">
-          <Cropper
-            :aspectRatio="1"
-            :width="256"
-            :height="256"
-            @crop="setPhoto"
-            :src="form.photo"
-          />
-        </el-form-item>
-        <el-form-item label="简介" prop="description">
-          <el-input
-            v-model="form.description"
-            placeholder="请输入简介"
-            type="textarea"
-          ></el-input>
-        </el-form-item>
-        <el-form-item>
-          <el-button type="primary" @click="submit">提交</el-button>
-        </el-form-item>
-      </el-form>
+      <el-tabs v-model="activeName">
+        <el-tab-pane label="资料修改" name="profile">
+          <el-form
+            :model="form"
+            :rules="rules"
+            ref="formRef"
+            label-width="80px"
+          >
+            <el-form-item label="昵称" prop="nickname">
+              <el-input
+                v-model="form.nickname"
+                placeholder="请输入昵称"
+              ></el-input>
+            </el-form-item>
+            <el-form-item label="邮箱" prop="email">
+              <el-input
+                v-model="form.email"
+                placeholder="请输入邮箱"
+              ></el-input>
+            </el-form-item>
+            <el-form-item label="头像" prop="photo">
+              <Cropper
+                :aspectRatio="1"
+                :width="256"
+                :height="256"
+                @crop="setPhoto"
+                :src="form.photo"
+              />
+            </el-form-item>
+            <el-form-item label="简介" prop="description">
+              <el-input
+                v-model="form.description"
+                placeholder="请输入简介"
+                type="textarea"
+              ></el-input>
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary" @click="submit">提交</el-button>
+            </el-form-item>
+          </el-form>
+        </el-tab-pane>
+        <el-tab-pane label="密码修改" name="password">
+          <el-form
+            :model="formPassword"
+            :rules="rulesPassword"
+            ref="formRefPassword"
+            label-width="80px"
+          >
+            <el-form-item label="原密码" prop="currentPassword">
+              <el-input
+                v-model="formPassword.currentPassword"
+                placeholder="如需更改密码，请输入原密码"
+                type="password"
+                show-password
+              ></el-input>
+            </el-form-item>
+            <el-form-item label="密码" prop="password">
+              <el-input
+                v-model="formPassword.password"
+                placeholder="如需更改密码，请输入新密码"
+                type="password"
+                show-password
+              ></el-input>
+            </el-form-item>
+            <el-form-item label="确认密码" prop="confirmPassword">
+              <el-input
+                v-model="formPassword.confirmPassword"
+                placeholder="如需更改密码，请再次输入新密码"
+                type="password"
+                show-password
+              ></el-input>
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary" @click="submitPassword">提交</el-button>
+            </el-form-item>
+          </el-form>
+        </el-tab-pane>
+      </el-tabs>
     </div>
-    <el-divider />
-    <h3 class="common-title">密码修改</h3>
-    <el-form
-      :model="formPassword"
-      :rules="rulesPassword"
-      ref="formRefPassword"
-      label-width="80px"
-    >
-      <el-form-item label="原密码" prop="currentPassword">
-        <el-input
-          v-model="formPassword.currentPassword"
-          placeholder="如需更改密码，请输入原密码"
-          type="password"
-          show-password
-        ></el-input>
-      </el-form-item>
-      <el-form-item label="密码" prop="password">
-        <el-input
-          v-model="formPassword.password"
-          placeholder="如需更改密码，请输入新密码"
-          type="password"
-          show-password
-        ></el-input>
-      </el-form-item>
-      <el-form-item label="确认密码" prop="confirmPassword">
-        <el-input
-          v-model="formPassword.confirmPassword"
-          placeholder="如需更改密码，请再次输入新密码"
-          type="password"
-          show-password
-        ></el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="submitPassword">提交</el-button>
-      </el-form-item>
-    </el-form>
   </div>
 </template>
 <script>
@@ -81,6 +96,7 @@ export default {
     const route = useRoute()
     const router = useRouter()
 
+    const activeName = ref('profile')
     const formRef = ref(null)
     const formRefPassword = ref(null)
     const form = reactive({
@@ -208,6 +224,7 @@ export default {
       setForm()
     })
     return {
+      activeName,
       formRef,
       formRefPassword,
       form,
