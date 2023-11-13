62ae07db0e2b8e12b1d47152892abc982a4285cf
hiroki <eeg1412@gmail.com>
2023-11-13 15:54:13 +0900

    附件压缩逻辑

--- server/api/admin/attachment/uploadAttachment.js
+++ server/api/admin/attachment/uploadAttachment.js
@@ -1,7 +1,100 @@
+const sharp = require('sharp');
+const path = require('path');
+const fs = require('fs')
 
 
 module.exports = async function (req, res, next) {
   const { file } = req
   const headers = req.headers
   const albumid = headers['albumid']
+  // 读取全局配置
+  const config = global.$globalConfig;
+  // // 开启图片压缩
+  // imgSettingEnableImgCompress: false,
+  // // 图片压缩质量
+  // imgSettingCompressQuality: 80,
+  // // 图片压缩最长边
+  // imgSettingCompressMaxSize: 1920,
+  // // 开启图片缩略图
+  // imgSettingEnableImgThumbnail: false,
+  // // 图片缩略图最长边
+  // imgSettingThumbnailMaxSize: 680,
+
+
+  /*
+   file 的内容如下:
+   {
+     fieldname: "file",
+     originalname: "fiename.jpg",
+     encoding: "7bit",
+     mimetype: "image/jpeg",
+     destination: "uploadCache",
+     filename: "3136fce19de1ccab50741c82acc62c4c",
+     path: "uploadCache\\3136fce19de1ccab50741c82acc62c4c",
+     size: 947083,
+   }
+   */
+
+  // TODO:album暂时写死
+  const filePath = path.join('./public/content/uploadfile/', 'album', file.filename)
+
+
+  try {
+    // 如果开启了图片缩略图
+    if (config.imgSettingEnableImgThumbnail && file.mimetype !== 'image/gif') {
+      const { imgSettingThumbnailMaxSize } = config
+      // 如果图片尺寸大于最长边
+      // 读取图片信息
+      const imageInfo = await sharp(file.path).metadata()
+      const { width, height } = imageInfo
+      const max = Math.max(width, height)
+      if (max > imgSettingThumbnailMaxSize) {
+        // 计算压缩比例
+        const scale = imgSettingThumbnailMaxSize / max
+        // 计算压缩后的宽高
+        const newWidth = Math.round(width * scale)
+        const newHeight = Math.round(height * scale)
+        // 压缩图片为webp 保存到 filePath 路径下
+        await sharp(file.path).resize(newWidth, newHeight).webp({ quality: 80 }).toFile(filePath + '_thumbnail')
+      }
+    }
+
+    if (config.imgSettingEnableImgCompress && file.mimetype !== 'image/gif') {
+      // 如果开启了图片压缩且图片不是gif
+      const { imgSettingCompressQuality, imgSettingCompressMaxSize } = config
+      // 如果图片尺寸大于最长边
+      // 读取图片信息
+      const imageInfo = await sharp(file.path).metadata()
+      const { width, height } = imageInfo
+      const max = Math.max(width, height)
+      if (max > imgSettingCompressMaxSize) {
+        // 计算压缩比例
+        const scale = imgSettingCompressMaxSize / max
+        // 计算压缩后的宽高
+        const newWidth = Math.round(width * scale)
+        const newHeight = Math.round(height * scale)
+        // 压缩图片为webp 保存到 filePath 路径下
+        await sharp(file.path).resize(newWidth, newHeight).webp({ quality: imgSettingCompressQuality }).toFile(filePath)
+      }
+    } else {
+      // 不压缩，直接将file.path的文件复制到filePath
+      fs.copyFileSync(file.path, filePath)
+    }
+    // 删除缓存文件
+    fs.unlinkSync(file.path)
+
+    // 发送响应
+    res.send({
+
+    })
+  } catch (err) {
+    console.error(err)
+    // 删除缓存文件
+    fs.unlinkSync(file.path)
+    res.status(400).json({
+      errors: [{
+        message: '文件上传失败'
+      }]
+    })
+  }
 }
