cba80a08d1abb1c7e7b69f0aa4061273a9c7829b
hiroki <eeg1412@gmail.com>
2023-11-16 12:53:53 +0900

    媒体列表

--- admin/src/components/AttachmentsDialog.vue
+++ admin/src/components/AttachmentsDialog.vue
@@ -5,6 +5,7 @@
     destroy-on-close
     :close-on-click-modal="false"
     align-center
+    class="attachments-dialog"
   >
     <template #header="{ close, titleId, titleClass }">
       <div class="my-header">
@@ -32,12 +33,13 @@
         </button>
       </div>
     </template>
-    <div>
+    <div class="attachments-dialog-body" v-loading="attachmentsLoading">
       <el-upload
         class="upload-demo"
         drag
         action="/api/admin/attachment/upload"
         multiple
+        v-model:file-list="fileList"
         :accept="'image/*'"
         :headers="headers"
         :on-success="handleSuccess"
@@ -46,20 +48,40 @@
         <el-icon class="el-icon--upload"><upload-filled /></el-icon>
         <div class="el-upload__text">拖动文件或点击上传</div>
       </el-upload>
-      <div>
-        <div
-          class="dib"
-          v-for="(item, index) in attachmentList"
-          :key="item._id"
-        >
-          <el-image
-            :src="item.thumfor || item.filepath"
-            fit="cover"
-            loading="lazy"
-            :preview-src-list="[item.filepath]"
-            style="width: 100px; height: 100px"
-          />
-        </div>
+      <div
+        class="mt15 custom-scroll scroll-not-hide attachments-list-body clearfix"
+      >
+        <template v-if="attachmentList.length > 0">
+          <div
+            class="fl attachment-item"
+            v-for="(item, index) in attachmentList"
+            :key="item._id"
+          >
+            <el-image
+              :src="item.thumfor || item.filepath"
+              fit="cover"
+              loading="lazy"
+              :preview-src-list="[item.filepath]"
+              style="width: 100%; height: 100px"
+            />
+          </div>
+        </template>
+        <template v-else>
+          <div class="full-height dflex flexCenter">
+            <el-empty description="暂无媒体文件，请选择其他相册" />
+          </div>
+        </template>
+      </div>
+      <!-- 分页 -->
+      <div class="clearfix mt10">
+        <el-pagination
+          class="fr"
+          background
+          layout="prev, pager, next"
+          :total="total"
+          v-model:page-size="params.size"
+          v-model:current-page="params.page"
+        />
       </div>
     </div>
   </el-dialog>
@@ -81,6 +103,7 @@ export default {
   emits: ['success', 'error', 'paramsChange'],
   setup(props, { emit }) {
     const visible = ref(false)
+    const fileList = ref([])
     const albumId = ref('')
     const open = () => {
       attachmentList.value = []
@@ -122,7 +145,7 @@ export default {
       clearTimeout(getAttachmentListTimer)
       getAttachmentListTimer = setTimeout(() => {
         getAttachmentList()
-      }, 1000)
+      }, 500)
     }
 
     const handleError = (err) => {
@@ -130,15 +153,17 @@ export default {
       emit('error')
     }
 
+    const attachmentsLoading = ref(false)
     const attachmentList = ref([])
     const params = reactive({
       page: 1,
-      size: 10,
+      size: 20,
       keyword: '',
       album: albumId.value,
     })
     const total = ref(0)
     const getAttachmentList = (resetPage) => {
+      attachmentsLoading.value = true
       if (resetPage) {
         params.page = 1
       }
@@ -152,6 +177,9 @@ export default {
         .catch((err) => {
           console.log(err)
         })
+        .finally(() => {
+          attachmentsLoading.value = false
+        })
     }
 
     const changeAlbum = () => {
@@ -160,23 +188,109 @@ export default {
       updateHeaders()
     }
 
+    // watch fileList
+    watch(
+      fileList,
+      (newVal, oldVal) => {
+        console.log(newVal, oldVal)
+        clearSuccessFileList()
+      },
+      { deep: true }
+    )
+
+    let clearSuccessFileListTimer = null
+    const clearSuccessFileList = () => {
+      clearTimeout(clearSuccessFileListTimer)
+      clearSuccessFileListTimer = setTimeout(() => {
+        // 清除掉status: "success" 的file
+        fileList.value = fileList.value.filter((item) => {
+          return item.status !== 'success'
+        })
+      }, 500)
+    }
+
+    // 监听 params.page 的变化
+    watch(
+      () => params.page,
+      (newVal, oldVal) => {
+        getAttachmentList()
+      }
+    )
+
     onMounted(() => {})
     onBeforeMount(() => {
       clearTimeout(getAttachmentListTimer)
     })
     return {
       visible,
+      fileList,
       albumId,
       open,
       albumList,
+      params,
+      total,
       headers,
       updateHeaders,
       handleSuccess,
       handleError,
       changeAlbum,
+      attachmentsLoading,
       attachmentList,
     }
   },
 }
 </script>
-<style scoped></style>
+<style scoped>
+.attachment-item {
+  width: 20%;
+}
+.attachments-list-body {
+  height: calc(100vh - 550px);
+  min-height: 200px;
+  overflow: auto;
+}
+/* 小于500 */
+@media screen and (max-width: 500px) {
+  .attachment-item {
+    width: 50%;
+  }
+}
+</style>
+
+<style>
+.attachments-dialog {
+  width: 800px;
+}
+.attachments-dialog .el-dialog__body {
+  padding-top: 0;
+}
+.attachments-dialog .el-upload-list {
+  max-height: 100px;
+  overflow-y: auto;
+  overflow-x: hidden;
+}
+.attachments-dialog .el-upload-list::-webkit-scrollbar {
+  width: 6px;
+  height: 6px;
+}
+
+.attachments-dialog .el-upload-list::-webkit-scrollbar-thumb {
+  height: 40px;
+  border-radius: 4px;
+  background-color: #bbb;
+}
+.attachments-dialog .el-upload-list:hover::-webkit-scrollbar-thumb {
+  height: 40px;
+  border-radius: 4px;
+  background-color: #bbb;
+}
+
+.attachments-dialog .el-upload-list::-webkit-scrollbar-thumb:hover {
+  background-color: #bbb;
+}
+.attachments-dialog .el-upload-list.scroll-not-hide::-webkit-scrollbar-thumb {
+  height: 40px;
+  border-radius: 4px;
+  background-color: #bbb;
+}
+</style>
