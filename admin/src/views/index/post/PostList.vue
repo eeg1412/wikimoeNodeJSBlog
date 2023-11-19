2375830d865a1337d27b005eb1834b02c640c818
hiroki <eeg1412@gmail.com>
2023-11-19 12:43:03 +0900

    添加一行注释

--- admin/src/views/index/post/PostList.vue
+++ admin/src/views/index/post/PostList.vue
@@ -2,6 +2,17 @@
   <div>postlist</div>
 </template>
 <script>
-export default {}
+export default {
+  setup() {
+    // TODO: img标签自动添加loading="lazy"
+    // let htmlContent = '你的HTML内容'
+    // htmlContent = htmlContent.replace(
+    //   /<img(?!.*?loading\s*=\s*['"]lazy['"])([^>]*?)>/gi,
+    //   '<img loading="lazy" $1>'
+    // )
+    // console.log(htmlContent)
+    return {}
+  },
+}
 </script>
 <style lang=""></style>
