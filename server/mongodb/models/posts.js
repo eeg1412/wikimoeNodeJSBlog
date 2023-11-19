bab626b79c18b32080b91ace84cb03fad41d5641
hiroki <eeg1412@gmail.com>
2023-11-19 21:37:11 +0900

    创建博客/推文/页面的表

---
+++ server/mongodb/models/posts.js
@@ -0,0 +1,104 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+// Schema
+var posts = new Schema({
+  //   - title	标题字段
+  // - date	日期字段
+  // - content	内容字段
+  // - excerpt	摘要字段
+  // - alias	别名字段
+  // - author	作者字段
+  // - sort	分类
+  // - type	类型：1blog,2tweet,3page
+  // - tags	标签字段[]
+  // - views	查看次数字段
+  // - comnum	评论次数字段
+  // - top	是否置顶字段
+  // - sortop	是否排序置顶字段
+  // - status	状态字段：0草稿，1发布，99回收站
+  // - allowRemark	是否允许评论字段
+  // - template	模板字段
+  // - code	文章插入的Code字段
+  // - coverImages  博客时是封面图片字段，页面时是页面图片字段
+
+  title: {
+    type: String,
+    default: ''
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  content: {
+    type: String,
+    default: ''
+  },
+  excerpt: {
+    type: String,
+    default: ''
+  },
+  alias: {
+    type: String,
+    default: '',
+    // 唯一
+    unique: true
+  },
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  sort: {
+    type: Schema.Types.ObjectId,
+    ref: 'sorts'
+  },
+  type: {
+    type: Number,
+  },
+  tags: {
+    // tag的id
+    type: Array,
+    default: []
+  },
+  views: {
+    type: Number,
+    default: 0
+  },
+  comnum: {
+    type: Number,
+    default: 0
+  },
+  top: {
+    type: Boolean,
+    default: false
+  },
+  sortop: {
+    type: Boolean,
+    default: false
+  },
+  status: {
+    type: Number,
+    default: 0
+  },
+  allowRemark: {
+    type: Boolean,
+    default: true
+  },
+  template: {
+    type: String,
+    default: ''
+  },
+  code: {
+    type: String,
+    default: ''
+  },
+  coverImages: {
+    type: Array,
+    default: []
+  },
+  updatetime: {
+    type: Date,
+    default: Date.now
+  },
+});
+
+module.exports = mongoose.model('posts', posts);
---
+++ server/mongodb/utils/posts.js
@@ -0,0 +1,47 @@
+const postsModel = require('../models/posts');
+
+exports.save = async function (parmas) {
+  // document作成
+  const posts = new postsModel(parmas);
+  // document保存
+  return await posts.save()
+}
+
+
+exports.findOne = async function (parmas) {
+  // document查询
+  return await postsModel.findOne(parmas);
+}
+
+// 查找所有
+exports.find = async function (parmas, sort) {
+  // document查询
+  return await postsModel.find(parmas).sort(sort);
+}
+
+// 分页查询
+exports.findPage = async function (parmas, sort, page, limit) {
+  // document查询
+  const list = await postsModel.find(parmas).sort(sort).skip((page - 1) * limit).limit(limit);
+  const total = await postsModel.countDocuments(parmas);
+  // 查询失败
+  if (!list || total === undefined) {
+    throw new Error('查询失败')
+  }
+  return {
+    list,
+    total
+  }
+}
+
+exports.updateOne = async function (filters, parmas) {
+  // document查询
+  parmas.$inc = { __v: 1, ...parmas.$inc }
+  parmas.updatetime = new Date()
+  return await postsModel.updateOne(filters, parmas);
+}
+// 删除
+exports.deleteOne = async function (filters) {
+  // document查询
+  return await postsModel.deleteOne(filters);
+}
