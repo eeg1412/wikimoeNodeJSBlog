bce7097f9033b97463ff2dea65a3f11a71251a94
hiroki <eeg1412@gmail.com>
2023-11-23 14:25:38 +0900

    添加评论表

---
+++ server/mongodb/models/comments.js
@@ -0,0 +1,52 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+// Schema
+var comments = new Schema({
+  // 文章id
+  post: {
+    type: Schema.Types.ObjectId,
+    ref: 'posts'
+  },
+  // 父评论id
+  parent: {
+    type: Schema.Types.ObjectId,
+    ref: 'comments'
+  },
+  // 评论日期
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  // 评论内容,对应emlog的comment
+  content: {
+    type: String,
+    default: ''
+  },
+  // 评论者昵称,对应emlog的poster
+  nickname: {
+    type: String,
+    default: ''
+  },
+  // 评论者邮箱
+  email: {
+    type: String,
+    default: ''
+  },
+  // 评论者网址
+  url: {
+    type: String,
+    default: ''
+  },
+  // 评论者ip
+  ip: {
+    type: String,
+    default: ''
+  },
+  // 评论状态,0待审核,1已审核,2未通过,查询评论时注意父级评论的状态
+  status: {
+    type: Number,
+    default: 0
+  },
+});
+
+module.exports = mongoose.model('comments', comments);
---
+++ server/mongodb/utils/comments.js
@@ -0,0 +1,46 @@
+const commentsModel = require('../models/comments');
+
+exports.save = async function (parmas) {
+  // document作成
+  const comments = new commentsModel(parmas);
+  // document保存
+  return await comments.save()
+}
+
+
+exports.findOne = async function (parmas) {
+  // document查询
+  return await commentsModel.findOne(parmas);
+}
+
+// 查找所有
+exports.find = async function (parmas, sort) {
+  // document查询
+  return await commentsModel.find(parmas).sort(sort);
+}
+
+// 分页查询
+exports.findPage = async function (parmas, sort, page, limit) {
+  // document查询
+  const list = await commentsModel.find(parmas).sort(sort).skip((page - 1) * limit).limit(limit);
+  const total = await commentsModel.countDocuments(parmas);
+  // 查询失败
+  if (!list || total === undefined) {
+    throw new Error('查询失败')
+  }
+  return {
+    list,
+    total
+  }
+}
+
+exports.updateOne = async function (filters, parmas) {
+  // document查询
+  parmas.$inc = { __v: 1, ...parmas.$inc }
+  return await commentsModel.updateOne(filters, parmas);
+}
+// 删除
+exports.deleteOne = async function (filters) {
+  // document查询
+  return await commentsModel.deleteOne(filters);
+}
