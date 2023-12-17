6c7238e981fcdb5525a511abfc5541933a9c7351
hiroki <eeg1412@gmail.com>
2023-12-17 15:26:26 +0900

    增加404页面

---
+++ blog/pages/404.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="error-body">
+    <div class="error-code">404</div>
+    <div class="error-msg">页面不见了？</div>
+    <!-- 尝试回到首页 -->
+    <div class="error-btn pointer" @click="reflushHome">重试</div>
+  </div>
+</template>
+<script setup>
+const reflushHome = () => {
+  window.location.href = '/'
+}
+</script>
+<style scoped>
+/* 报错页面式样，画面居中显示，code粉色 */
+.error-body {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  /* 换行显示 colums*/
+  flex-direction: column;
+
+  justify-content: center;
+  align-items: center;
+  background: #ffffff;
+  height: 100dvh;
+}
+.error-code {
+  font-size: 100px;
+  color: #ef90a7;
+}
+.error-msg {
+  font-size: 20px;
+  color: #999;
+}
+.error-btn {
+  padding: 10px 20px;
+  border-radius: 20px;
+  background: #ef90a7;
+  color: #ffffff;
+  display: block;
+  margin-top: 20px;
+}
+</style>
