c1748a8c2cd54683450a8f289a79206e6d7eedc8
hiroki <eeg1412@gmail.com>
2023-12-25 13:29:02 +0900

    更改创建工具

--- server/tools/createUser.js
+++ server/tools/createUser.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 // envType: 本番：prod 开发：dev
 // const envType = process.argv[3] || 'dev'
 // dotenv.config({ path: `./${envType}.env` })
-const db = require('../mongodb/index')
+const db = require('./mongodb')
 const userUtils = require('../mongodb/utils/users')
 const chalk = require('chalk')
 const { Select, Form } = require('enquirer')
---
+++ server/tools/mongodb.js
@@ -0,0 +1,29 @@
+var mongoose = require('mongoose');
+console.info('数据库连接中...');
+// console.log('数据库地址：', process.env.DB_HOST);
+if (!process.env.DB_HOST) {
+  console.error('请在根目录下创建.env文件，并添加数据库地址。');
+  process.exit(1);
+}
+mongoose.connect(process.env.DB_HOST, { useNewUrlParser: true, useUnifiedTopology: true });
+var db = mongoose.connection;
+
+db.once('open', async () => {
+  console.info('数据库连接成功！');
+})
+
+db.on('error', function (error) {
+  console.error(
+    'Error in MongoDb connection: ' + error
+  );
+  mongoose.disconnect();
+});
+
+db.on('close', function () {
+  console.error(
+    '数据库断开，重新连接数据库'
+  );
+  mongoose.connect(process.env.DB_HOST, { useNewUrlParser: true, useUnifiedTopology: true });
+});
+
+module.exports = db;
