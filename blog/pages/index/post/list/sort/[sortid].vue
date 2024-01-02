481943763a09e12dae65b424bf0e2e9f3544a8d6
hiroki <eeg1412@gmail.com>
2024-01-02 17:07:16 +0900

    样式优化

--- blog/pages/index/post/list/sort/[sortid].vue
+++ blog/pages/index/post/list/sort/[sortid].vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div
-      class="text-primary-400 pt-2 pb-2 block text-center border-solid border-b border-gray-200"
+      class="text-primary-400 p-2 pb-2 pl-4 pr-4 block text-center border-solid border-b border-gray-200"
     >
       分类：{{ data.sortname }}
     </div>
