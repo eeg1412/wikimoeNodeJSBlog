2c7e56ce90138a1b84036149d19ef2f3f9181f46
eeg1412 <eeg1412@gmail.com>
2024-05-17 15:38:43 +0900

    快捷创建

--- server/tools/createUser.js
+++ server/tools/createUser.js
@@ -27,50 +27,7 @@ const checkAdmin = async (username, nickname, role) => {
     return res
 }
 
-
-const creatAdmin = async () => {
-    const prompt = new Form({
-        name: 'username',
-        message: hasAdmin ? '创建管理员:' : '创建博客站长:',
-        choices: [
-            { name: 'username', message: '用户名' },
-            { name: 'nickname', message: '昵称' },
-            { name: 'password', message: '密码' },
-        ],
-        validate: async (value) => {
-            console.log(value)
-            let { username, password, nickname } = value
-            if (!username) {
-                return '请输入用户名'
-            }
-            if (!password) {
-                return '请输入密码'
-            }
-            // 管理账号小写
-            // username = username.toLowerCase()
-            const passwordReg = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[a-zA-Z\d!@#$%^&*]{4,}$/
-            if (!passwordReg.test(password)) {
-                return '密码必须4位以上且包含大小写、数字和符号（!@#$%^&*）'
-            }
-            const userNameReg = /^[a-z0-9]+$/
-            if (!userNameReg.test(username)) {
-                return '用户名仅支持半角小写英文和数字'
-            }
-            // 校验用户名是否存在
-            const userRes = await checkAdmin(username)
-            if (userRes) {
-                return '该用户名已存在'
-            }
-            // 校验昵称是否存在
-            const nicknameRes = await checkAdmin(null, nickname)
-            if (nicknameRes) {
-                return '该昵称已存在'
-            }
-
-            return true
-        }
-    })
-    const res = await prompt.run()
+const saveToDB = async (res) => {
     const username = res.username.toLowerCase()
     const params = {
         username: username,
@@ -82,6 +39,54 @@ const creatAdmin = async () => {
     console.log(
         chalk.green(`${username}添加成功`)
     )
+}
+const checkForm = async (value) => {
+    console.log(value)
+    let { username, password, nickname } = value
+    if (!username) {
+        return '请输入用户名'
+    }
+    if (!password) {
+        return '请输入密码'
+    }
+    // 管理账号小写
+    // username = username.toLowerCase()
+    const passwordReg = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[a-zA-Z\d!@#$%^&*]{4,}$/
+    if (!passwordReg.test(password)) {
+        return '密码必须4位以上且包含大小写、数字和符号（!@#$%^&*）'
+    }
+    const userNameReg = /^[a-z0-9]+$/
+    if (!userNameReg.test(username)) {
+        return '用户名仅支持半角小写英文和数字'
+    }
+    // 校验用户名是否存在
+    const userRes = await checkAdmin(username)
+    if (userRes) {
+        return '该用户名已存在'
+    }
+    // 校验昵称是否存在
+    const nicknameRes = await checkAdmin(null, nickname)
+    if (nicknameRes) {
+        return '该昵称已存在'
+    }
+
+    return true
+}
+const creatAdmin = async () => {
+    const prompt = new Form({
+        name: 'username',
+        message: hasAdmin ? '创建管理员:' : '创建博客站长:',
+        choices: [
+            { name: 'username', message: '用户名' },
+            { name: 'nickname', message: '昵称' },
+            { name: 'password', message: '密码' },
+        ],
+        validate: async (value) => {
+            return await checkForm(value)
+        }
+    })
+    const res = await prompt.run()
+    await saveToDB(res)
     process.exit(0)
 }
 
@@ -93,7 +98,25 @@ const init = async () => {
         )
         hasAdmin = true
     }
-    creatAdmin()
+    // 如果有3个参数
+    if (process.argv.length === 5) {
+        const value = {
+            username: process.argv[2],
+            password: process.argv[3],
+            nickname: process.argv[4],
+        }
+        const checkRes = await checkForm(value)
+        if (checkRes === true) {
+            await saveToDB(value)
+        } else {
+            console.log(
+                chalk.red(checkRes)
+            )
+        }
+        process.exit(0)
+    } else {
+        creatAdmin()
+    }
 }
 
 db.once('open', () => {
