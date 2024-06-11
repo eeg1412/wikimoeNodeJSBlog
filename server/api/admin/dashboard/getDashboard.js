60dbc8c71d62c6fe481e090252d226360a3aa49e
timochan <i@timochan.cn>
2024-06-11 19:03:57 +0800

    fix: typo

--- server/api/admin/dashboard/getDashboard.js
+++ server/api/admin/dashboard/getDashboard.js
@@ -53,8 +53,8 @@ module.exports = async function (req, res, next) {
   // JSON_LIMT="50mb"
   // URLENCODED_LIMT="50mb"
   // 返回以上数据
-  const jsonLimit = process.env.JSON_LIMT || '10mb'
-  const urlencodedLimit = process.env.URLENCODED_LIMT || '10mb'
+  const jsonLimit = process.env.JSON_LIMIT || '10mb'
+  const urlencodedLimit = process.env.URLENCODED_LIMIT || '10mb'
   // 获取nodejs版本
   const nodeVersion = process.version
   // 获取系统信息
