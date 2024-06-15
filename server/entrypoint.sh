5773d967a23c13f433a0d2f79718957802d8f710
hiroki <eeg1412@gmail.com>
2024-06-15 11:07:06 +0900

    判断log文件的存在

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -8,6 +8,10 @@ function start() {
 function init() {
     echo "Initializing user: $USER_NAME"
     yarn run create-user "$USER_NAME" '7@wVUo6BL6LHjNR*#x' "$USER_NAME"
+    # 检查/app/log目录是否存在，如果不存在，则创建它
+    if [ ! -d "/app/log" ]; then
+        mkdir -p "/app/log"
+    fi
     touch "$LOCK_FILE"
 }
 if [ -n "$USER_NAME" ]; then
