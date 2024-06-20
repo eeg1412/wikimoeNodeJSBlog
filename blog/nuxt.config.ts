ce3cf53e3a3c1849dc4693249ba9ade6a733bf02
hiroki <eeg1412@gmail.com>
2024-06-20 17:41:12 +0900

    暂不启用页面切换动画

--- blog/nuxt.config.ts
+++ blog/nuxt.config.ts
@@ -100,7 +100,7 @@ export default defineNuxtConfig({
       viewport:
         'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0',
     },
-    pageTransition: { name: 'page', mode: 'out-in' },
+    // pageTransition: { name: 'page', mode: 'out-in' },
   },
   devtools: { enabled: true },
   devServer: {
