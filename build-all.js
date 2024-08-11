03ee53528ebe43cbad119eb3242e512a5e19aa3f
hiroki <eeg1412@gmail.com>
2024-08-11 14:22:37 +0900

    增加一键编译

---
+++ build-all.js
@@ -0,0 +1,7 @@
+const { execSync } = require('child_process');
+console.log('install and build admin');
+execSync('cd admin && yarn install && yarn build && cd ..', { stdio: 'inherit' });
+console.log('install server');
+execSync(`cd server && yarn install && cd ..`, { stdio: 'inherit' });
+console.log('install and build blog');
+execSync('cd blog && yarn install && yarn build && cd ..', { stdio: 'inherit' });
