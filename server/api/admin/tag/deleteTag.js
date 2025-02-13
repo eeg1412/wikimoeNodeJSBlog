b520e45550978b710c03f1dfa53932befff22706
hiroki <eeg1412@gmail.com>
2025-02-13 15:37:27 +0900

    添加删除标签时更新文章中的标签逻辑，确保标签被正确移除

--- server/api/admin/tag/deleteTag.js
+++ server/api/admin/tag/deleteTag.js
@@ -1,4 +1,5 @@
 const tagUtils = require('../../../mongodb/utils/tags')
+const postUtils = require('../../../mongodb/utils/posts')
 const utils = require('../../../utils/utils')
 const log4js = require('log4js')
 const adminApiLog = log4js.getLogger('adminApi')
@@ -23,10 +24,25 @@ module.exports = async function (req, res, next) {
       })
       return
     }
-    res.send({
-      data: {
-        message: '删除成功'
+    // 删除文章中的tag
+    postUtils.updateMany({ tags: id }, {
+      $pull: {
+        tags: id
       }
+    }).then((postData) => {
+      // console.log(postData)
+      res.send({
+        data: {
+          message: '删除成功'
+        }
+      })
+    }).catch((err) => {
+      res.status(400).json({
+        errors: [{
+          message: '删除失败'
+        }]
+      })
+      adminApiLog.error(`tag delete fail, ${logErrorToText(err)}`)
     })
     // utils.reflushBlogCache()
   }).catch((err) => {
