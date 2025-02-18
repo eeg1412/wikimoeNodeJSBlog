892d6874c1fb6b7cecf336bf2cf49008b0b0154a
hiroki <eeg1412@gmail.com>
2025-02-18 11:00:37 +0900

    优化PostACG组件的样式，调整网格布局和响应式设计

--- blog/components/PostACG.vue
+++ blog/components/PostACG.vue
@@ -6,19 +6,22 @@
       相关作品：
     </div>
     <div
-      class="grid gap-4"
+      class="grid gap-3 post-acg-grid"
       :class="{
-        'grid-cols-1': contentCount === 1,
-        'xl:grid-cols-2': contentCount > 1,
+        odd: contentCount % 2 === 1,
       }"
     >
-      <div v-for="bangumi in bangumiList" :key="bangumi._id" class="mb-1">
+      <div
+        v-for="bangumi in bangumiList"
+        :key="bangumi._id"
+        class="post-acg-item"
+      >
         <BangumiItem :bangumi="bangumi" />
       </div>
-      <div v-for="book in bookList" :key="book._id" class="mb-1">
+      <div v-for="book in bookList" :key="book._id" class="post-acg-item">
         <BookItem :book="book" />
       </div>
-      <div v-for="game in gameList" :key="game._id" class="mb-1">
+      <div v-for="game in gameList" :key="game._id" class="post-acg-item">
         <GameItem :game="game" />
       </div>
     </div>
@@ -51,4 +54,16 @@ const contentCount = computed(() => {
   )
 })
 </script>
-<style scoped></style>
+<style scoped>
+.post-acg-grid {
+  grid-template-columns: repeat(1, minmax(0, 1fr));
+}
+@media (min-width: 1280px) {
+  .post-acg-grid {
+    grid-template-columns: repeat(2, minmax(0, 1fr));
+  }
+  .post-acg-grid.odd .post-acg-item:last-child {
+    grid-column: span 2 / span 2;
+  }
+}
+</style>
