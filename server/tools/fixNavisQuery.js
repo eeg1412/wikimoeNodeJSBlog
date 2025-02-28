82392722825a7ee9d4fc4021baa1b88a90ba1bce
hiroki <eeg1412@gmail.com>
2025-02-28 23:40:44 +0900

    添加修复导航query字段类型的工具脚本

---
+++ server/tools/fixNavisQuery.js
@@ -0,0 +1,38 @@
+require('dotenv').config()
+const chalk = require('chalk')
+const db = require('./mongodb')
+const navisUtils = require('../mongodb/utils/navis');
+
+const init = async () => {
+  console.log(chalk.green('开始修复导航query字段'))
+  // 查找所有 query 字段为对象类型的文档
+  const docs = await navisUtils.find({
+    query: { $type: 'object' }
+  }, undefined, undefined, { lean: true });
+  console.log(`找到 ${docs.length} 个需要更新的文档`);
+
+  // 逐个更新文档
+  for (const doc of docs) {
+    if (doc.query && typeof doc.query === 'object') {
+      // 将对象转换为字符串
+      const queryString = '';
+
+      // 更新该文档
+      await navisUtils.updateOne(
+        { _id: doc._id },
+        { $set: { query: queryString } },
+        {
+          strict: false, // 忽略架构验证
+          new: true      // 返回更新后的文档
+        }
+      );
+    }
+  }
+
+  console.log('更新完成');
+  process.exit(0)
+}
+
+db.once('open', () => {
+  init()
+})
