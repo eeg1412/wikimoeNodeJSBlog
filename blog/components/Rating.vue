fb0e966f9cbb1cfd354c911c2137569f75401d0b
hiroki <eeg1412@gmail.com>
2025-03-04 14:51:33 +0900

    【博客端】修改ACG页面的评分分割线的样式

--- blog/components/Rating.vue
+++ blog/components/Rating.vue
@@ -2,14 +2,14 @@
   <div class="rating-body">
     <div
       v-if="rating"
-      class="text-sm text-primary border border-solid border-primary-400 text-center rounded"
+      class="text-sm text-primary border border-solid border-primary-400 text-center rounded flex items-center justify-center"
     >
-      <span>{{ rating }}</span
-      >分 | {{ ratingToText(rating) }}
+      <span>{{ rating }}分</span><span class="rating-bar"></span
+      ><span>{{ ratingToText(rating) }}</span>
     </div>
     <div
       v-else
-      class="text-sm text-gray-300 border border-solid border-gray-300 text-center rounded"
+      class="text-sm text-gray-300 border border-solid border-gray-300 text-center rounded flex items-center justify-center"
     >
       暂无评分
     </div>
@@ -26,5 +26,12 @@ const props = defineProps({
 <style scoped>
 .rating-body {
   margin-top: 9px;
+}
+.rating-bar {
+  @apply bg-primary-400;
+  display: inline-block;
+  width: 1px;
+  height: 0.875rem;
+  margin: 0 0.36rem;
 }
 </style>
