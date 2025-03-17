c172c8c96acfd3a5828315437865b38dd4170aa3
hiroki <eeg1412@gmail.com>
2025-03-17 16:59:58 +0900

    【博客端】优化活动列表点击行为，避免重复加载事件详情

--- blog/components/PostAboutEvent.vue
+++ blog/components/PostAboutEvent.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="eventList.length > 0" class="mt-3 mb-3 relative" @click.stop>
+  <div v-if="eventList.length > 0" class="mt-3 mb-3 relative">
     <div
       v-if="showTitle"
       class="mb-3 text-gray-600 font-bold dark:text-gray-200 text-base border-b border-dotted pb-2 border-gray-300 dark:border-gray-700"
@@ -12,18 +12,19 @@
           v-for="event in eventList"
           :key="event._id"
           :id="`event-${idPrefix}-${event._id}-${postId}`"
-          class="text-sm text-primary-500 pointer mb-2 header-scroll-margin-top"
-          @click="getEventDetail(event._id)"
+          class="text-sm text-primary-500 mb-2 header-scroll-margin-top"
         >
           <div class="overflow-hidden overflow-ellipsis whitespace-nowrap">
-            <span
-              v-if="event.eventtype"
-              class="post-event-about-event-type"
-              :style="{
-                backgroundColor: event.eventtype.color,
-              }"
-              >{{ event.eventtype.name }}</span
-            >{{ event.title }}
+            <span class="pointer" @click.stop="getEventDetail(event._id)"
+              ><span
+                v-if="event.eventtype"
+                class="post-event-about-event-type"
+                :style="{
+                  backgroundColor: event.eventtype.color,
+                }"
+                >{{ event.eventtype.name }}</span
+              >{{ event.title }}</span
+            >
           </div>
         </li>
       </ul>
@@ -61,6 +62,10 @@ const eventOpen = ref(false)
 const currentEventData = ref(null)
 const contentIsLoading = ref(false)
 const getEventDetail = async (id) => {
+  if (contentIsLoading.value) {
+    console.log('loading')
+    return
+  }
   contentIsLoading.value = true
   getEventDetailApiFetch({
     id,
