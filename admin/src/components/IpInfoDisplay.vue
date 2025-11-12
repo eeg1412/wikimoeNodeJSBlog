1285d3176d0a32351baef49cd4091d382d43ff08
eeg1412 <eeg1412@gmail.com>
2025-11-12 15:55:46 +0900

    优化 IpInfoDisplay 组件，增加工具提示以显示国家和地区的英文名称

--- admin/src/components/IpInfoDisplay.vue
+++ admin/src/components/IpInfoDisplay.vue
@@ -1,12 +1,31 @@
 <template>
   <div v-if="ipInfo">
-    {{ str }}
+    <div class="dflex flexMiddle">
+      <div>{{ str }}</div>
+
+      <el-tooltip
+        effect="dark"
+        placement="top"
+        :show-after="200"
+        max-width="400"
+        v-if="str !== englishStr"
+      >
+        <template #content>
+          <div class="tooltip-content">
+            {{ englishStr }}
+          </div>
+        </template>
+        <div class="ml5">
+          <el-link type="primary"><i class="fa fa-language f16"></i></el-link>
+        </div>
+      </el-tooltip>
+    </div>
   </div>
 </template>
 
 <script>
 import { getCountryData } from 'js-locations-zh'
-import { ref, watch } from 'vue'
+import { ref, watch, computed } from 'vue'
 
 export default {
   props: {
@@ -16,7 +35,16 @@ export default {
     }
   },
   setup(props) {
-    const str = ref('')
+    const str = ref('加载中...')
+
+    const englishStr = computed(() => {
+      if (!props.ipInfo) return ''
+      const { countryLong = '', region = '', city = '' } = props.ipInfo
+      if (!countryLong) return ''
+      return `${countryLong}${region !== city ? ' ' + region : ''}${
+        countryLong !== city ? ' ' + city : ''
+      }`
+    })
 
     watch(
       () => props.ipInfo,
@@ -52,7 +80,8 @@ export default {
     )
 
     return {
-      str
+      str,
+      englishStr
     }
   }
 }
